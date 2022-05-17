e02acd8a17925675dbc709aedafdb665a66067bf
parteek-5296 <105700137+parteek-5296@users.noreply.github.com>
2022-05-17 11:47:30 +0530

    Add files via upload

---
+++ assignment2 sol.py
@@ -0,0 +1,53 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\lenovo\Desktop\New folder\assignment2.py ==========
+-----------QUESTION 1-------------------
+----------Part a-------------------------
+35
+----------Part b-------------------------
+egaugnal evitisnes esac a si nohtyP
+----------Part c-------------------------
+a case sensitive
+----------Part d-------------------------
+Python is object oriented language
+----------Part e-------------------------
+10
+----------Part f-------------------------
+Pythonisacasesensitivelanguage
+
+-----------QUESTION 2-------------------
+Enter your name: Parteek Badwal
+Enter Your SID: 21102070
+Enter your department: Civil
+Enter your CGPA: 9.0
+Hey, Parteek Badwal Here! 
+My SID is 21102070
+I am from Civil department and my cg is 9.0
+
+-----------QUESTION 3-------------------
+----------Part a-------------------------
+8
+----------Part b-------------------------
+58
+----------Part c-------------------------
+50
+----------Part d-------------------------
+224 40
+----------Part e-------------------------
+14 0
+
+-----------QUESTION 4-------------------
+Enter the string: my name is parteek
+YES
+
+-----------QUESTION 5-------------------
+Enter side 1: 7
+Enter side 2: 5
+Enter side 3: 13
+YES
+
+-----------QUESTION 6-------------------
+Enter first number:4
+Enter second number:5
+1
